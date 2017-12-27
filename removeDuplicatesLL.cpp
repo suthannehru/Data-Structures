b1fd5e3812bfc114588f686ef29c2be010fdf385
suthanehru <suthanehrulingam@gmail.com>
2017-12-27 02:41:44 -0500

    C++ Remove Duplicates Linked List

---
+++ removeDuplicatesLL.cpp
@@ -0,0 +1,22 @@
+Node* RemoveDuplicates(Node *head)
+{
+   Node* temp = (Node*)malloc(sizeof(Node));
+   Node* prev = (Node*)malloc(sizeof(Node));
+   temp = head;
+   prev = head;
+   if(head == NULL){return head;}
+   int val = (*temp).data;
+   temp = (*temp).next;
+   while(temp != NULL){
+       if((*temp).data == val){
+           (*prev).next = (*temp).next;            
+       }
+       else{
+          val = (*temp).data;
+          prev = temp;    
+       }       
+       temp = (*temp).next;       
+   }
+    return head;
+    
+}
