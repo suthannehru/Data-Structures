2fb45464f46b72885d8dc988a6b02890cd0dd059
suthanehru <suthanehrulingam@gmail.com>
2017-12-27 02:07:33 -0500

    C++ Insert Node at tail of Linked List

---
+++ insertNodeatTailLinkedList.cpp
@@ -0,0 +1,20 @@
+Node* Insert(Node *head,int data)
+{
+  // Complete this method
+    Node* n = (Node*)malloc(sizeof(Node));
+    (*n).data = data;
+    (*n).next = NULL;
+    Node* temp = (Node*)malloc(sizeof(Node));
+    if(head == NULL){
+        head = n;
+        return head;
+    }
+    else{
+        temp = head;
+        while((*temp).next != NULL){
+            temp = (*temp).next;
+        }
+        (*temp).next = n;
+        return head;
+    }
+}
